6573013c6d9501e549f70ad9f6e7d148f305547f
Ana Tovalin <astrostar98@gmail.com>
2014-10-01 10:46:54 -0700

    Commit after starter milestone 10

--- app/src/main/java/android/tovalina/blogreader/BlogPostTask.java
+++ app/src/main/java/android/tovalina/blogreader/BlogPostTask.java
@@ -6,6 +6,8 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -14,9 +16,20 @@ public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject> {
     protected JSONObject doInBackground(Activity... activities) {
         try {
             URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=10"); //checks for error
+
+            HttpURLConnection connection = (HttpURLConnection)blogFeedUrl.openConnection(); //opens connection with URL
+            connection.connect();
+            int responseCode = connection.getResponseCode(); //gets response code
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+
+            }
         }
         catch(MalformedURLException error) { //catches error
             Log.e("BlogPostTask", "Malformed URL: " + error);
+        }
+        catch(IOException error) {
+            Log.e("BlogPostTask", "IO Exception", error);
         }
         return null;
     }
