95d5072473ce7f9decd034b3669fb88942aa0029
Ana Tovalin <astrostar98@gmail.com>
2014-10-02 15:00:47 -0700

    Commit after starter milestone 16

--- app/src/main/java/android/tovalina/blogreader/BlogPostParser.java
+++ app/src/main/java/android/tovalina/blogreader/BlogPostParser.java
@@ -35,8 +35,9 @@ public class BlogPostParser {
 
         try {
             while ((line = reader.readLine()) != null) {
-
+                builder.append(line);
             }
+            Log.i("BlogPostParser", "Input Stream String " + builder.toString());
         }
         catch (IOException error) {
             Log.e("BlogPostParser", "IOException " + error); //captures and displays error
