2388fedd1e2597bbc2c3e2b101354c781d1bf4e9
Ana Tovalin <astrostar98@gmail.com>
2014-10-03 14:50:07 -0700

    Commit after starter milestone 17

--- app/src/main/java/android/tovalina/blogreader/BlogPostParser.java
+++ app/src/main/java/android/tovalina/blogreader/BlogPostParser.java
@@ -2,12 +2,15 @@ package android.tovalina.blogreader;
 
 import android.util.Log;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.jar.JarException;
 
 /**
  * Created by Student on 10/2/2014.
@@ -37,10 +40,14 @@ public class BlogPostParser {
             while ((line = reader.readLine()) != null) {
                 builder.append(line);
             }
-            Log.i("BlogPostParser", "Input Stream String " + builder.toString());
+            JSONTokener jsonTokener = new JSONTokener(builder.toString()); //breaks down string into tokens
+            jsonObject = new JSONObject(jsonTokener);
         }
         catch (IOException error) {
             Log.e("BlogPostParser", "IOException " + error); //captures and displays error
+        }
+        catch (JSONException error) {
+            Log.e("BlogPostParser", "JSON Exception " + error);
         }
 
         return jsonObject;
