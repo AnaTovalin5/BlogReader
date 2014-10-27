204cc0dbc101226c3d90ad6218d5e9d2146089f4
Ana Tovalin <astrostar98@gmail.com>
2014-10-27 14:47:57 -0700

    Commit after intermediate milestone 4

--- app/src/main/java/android/tovalina/blogreader/BlogPostAdapter.java
+++ app/src/main/java/android/tovalina/blogreader/BlogPostAdapter.java
@@ -19,11 +19,14 @@ public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
         BlogPost post = getItem(position); //stores blogpost
 
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.blog_item, parent, false);
         }
 
-        TextView textView = (TextView)convertView.findViewById(android.R.id.text1);
-        textView.setText(post.title);
+        TextView title = (TextView)convertView.findViewById(R.id.title);
+        title.setText(post.title);
+
+        TextView author = (TextView)convertView.findViewById(R.id.author);
+        author.setText(post.author);
 
         return convertView;
     }
