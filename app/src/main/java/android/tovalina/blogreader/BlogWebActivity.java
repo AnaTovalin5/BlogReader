80f8d9efa90798eb11c232ac0c3aa54b778e3c67
Ana Tovalin <astrostar98@gmail.com>
2014-10-28 14:55:40 -0700

    Commit after intermediate milestone 7

--- app/src/main/java/android/tovalina/blogreader/BlogWebActivity.java
+++ app/src/main/java/android/tovalina/blogreader/BlogWebActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class BlogWebActivity extends Activity {
     @Override
@@ -16,8 +18,16 @@ public class BlogWebActivity extends Activity {
         Intent intent = getIntent(); //gets Intent Data
         Uri blogUri = intent.getData(); //gets Uri data from the intent
 
-        WebView webView = (WebView)findViewById(R.id.webView);
+        final WebView webView = (WebView)findViewById(R.id.webView);
         webView.loadUrl(blogUri.toString());
 
+        final ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressWebBar);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
